de7d6b0c4b37b18a13809b241d065ba00635e9a0
Guihgo <guihgo100milha@gmail.com>
2019-05-25 16:41:37 -0300

    update

--- 2019-1/javascript/codigo.js
+++ 2019-1/javascript/codigo.js
@@ -1,3 +1,9 @@
 window.onload = function () {
+    console.log('pagina carregada');
+    var botao = document.getElementById('btn');
+
+    botao.onclick = function () {
+        alert('clikou');
+    }
 
 }
